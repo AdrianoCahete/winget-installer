b23e5ad1d92fd106e43e55cd15701a4df7fcd65e
Adriano Cahete <me@adrianocahete.dev>
2020-05-20 17:44:56 -0300

    Add readme and fixes some install code

--- Install.ps1
+++ Install.ps1
@@ -1,9 +1,21 @@
-﻿# Install Winget
-# TODO: Install appxbundle - https://docs.microsoft.com/en-us/powershell/module/appx/add-appxpackage?redirectedfrom=MSDN&view=win10-ps
+﻿# =============================================================
+# Copyright 2020 Adriano Cahete <https://adrianocahete.dev/>
+# TODO: Add License
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================
+
+# Install Winget
+# TODO: Check windows version
 # TODO: Check if it's easier to get from repository or MS Store
 # TODO: Check if Sideloading is enabled - https://docs.microsoft.com/en-us/windows/uwp/get-started/enable-your-device-for-development
 # TODO: Do the option to enable sideloading from PS console (I don't know even it's possible)
-# TODO: Clear old files
+# TODO: Clear old files before start
+
 
 # Download latest release from github
 $Repo = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
@@ -17,19 +29,19 @@ $fileName = "$($APIResponse.name.Replace(" ","_")).appxbundle"
 $OutputPath = "$((Get-Location).Path)\files\$fileName"
 
 if (Test-Path -Path "$((Get-Location).Path)\files\")  {
-    pushd files
+    Push-Location files
     Write-Output "Downloading $fileName ...`n"
     Invoke-RestMethod -Method Get -Uri $FileUrl -OutFile $OutputPath
 } else {
-    mkdir files > $nul
-    pushd files
+    mkdir files > $null
+    Push-Location files
     Write-Output "Downloading $fileName ... `n"
     Invoke-RestMethod -Method Get -Uri $FileUrl -OutFile $OutputPath
 }
 
 Write-Output "`nInstalling $fileName ...`n"
 Add-AppxPackage $OutputPath
-popd
+Pop-Location
 refreshenv
 
 try {
---
+++ README.md
@@ -0,0 +1,22 @@
+# Winget Installer
+
+Winget Installer is a Powershell script to install [Windows Package Manager CLI](https://github.com/microsoft/winget-cli/)
+
+## This Install file isn't fully tested, so may not work in some machines.
+
+## Installation
+
+Run the `Install.ps1` file by downloading it or calling by Powershell console
+```
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://github/install.ps1'))
+```
+
+## Usage
+
+Usage follows the Winget parameters. See their documentations or run
+```
+winget --help
+```
+
+## License
+[?](https://choosealicense.com/licenses/)
